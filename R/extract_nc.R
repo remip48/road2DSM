adee4ea084d2076dd9365dba7821b899caa1de16
Pigeault <234028@tiho-hannover.de>
2025-10-23 10:46:17 +0200

    up

--- R/extract_nc.R
+++ R/extract_nc.R
@@ -648,7 +648,7 @@ extract_nc <- function (nc.path, list_variable, nc_files, all_pixel.radius,
               if (run_mean_SDspace) {
                 setDT(outM)
 
-                id_nc_table <- outM[, c(list(id_nc = id_nc[which.min(dist)])), by = list(id, t)]
+                id_nc_table <- outM[, c(list(id_nc = .SD$id_nc[which.min(.SD$dist)])), by = list(id, t), .SDcols = c("id_nc", "dist")]
 
                 if (all(c("SDspace", "mean") %in% pred.type)) {
                   outM <- outM[, c(list(id_nc = id_nc[which.min(dist)]),
