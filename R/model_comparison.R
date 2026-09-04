4e97d02fcc853d16cde130238f58b7aabe8823d0
Benoit <253135@tiho-hannover.de>
2026-09-04 14:35:37 +0200

    Better plots

--- R/model_comparison.R
+++ R/model_comparison.R
@@ -31,6 +31,7 @@
 #' @param n_cores
 #' @param title
 #' @param authors_markdown
+#' @param save_plots
 #'
 #' @return
 #' @importFrom foreach %dopar%
@@ -72,7 +73,8 @@ model_comparison <- function(run_models, # output from run_all_DSM
                              authors_markdown = NULL,
                              outfile = "log.txt",
                              save_posterior_distribution = F,
-                             n_cores = 1) {
+                             n_cores = 1,
+                             save_plots = FALSE) {
 
   type_model <- ifelse(any(class(run_models$best_models[[1]]) == "gam"),
                        "GAM",
@@ -216,6 +218,9 @@ model_comparison <- function(run_models, # output from run_all_DSM
   })
   ##############
 
+  best_stacking <- which(run_models$all_fits_binded[1:length(run_models$best_models), ]$stacking_weights
+                         == max(run_models$all_fits_binded[1:length(run_models$best_models), ]$stacking_weights))
+
   chunk_grpsize_model <- quote({
     if (model_grpsize) {
       cat("#### Group size model - ", ifelse(soap_use,
@@ -253,6 +258,78 @@ model_comparison <- function(run_models, # output from run_all_DSM
     "```"
   )
 
+  ### Modify and improve spline plot
+  build_spline_plot <- function(model, model4plotting, which_soap, log1p_trans) {
+    # Extract gratia plot to modify it
+    plot <- gratia::draw(model4plotting, rug = T, select = -which_soap)
+
+    # Extract edfs for each covariates
+    gam_summary <- summary(model)
+    edf_values <- gam_summary$s.table[, "edf"]
+
+    # Modify each subplots
+    for (n in 1:length(plot)){
+      current_covariate <- gsub("s\\((.*)\\)", "\\1", plot[[n]]$labels$title)
+
+      # Transform aspect and slope to degrees
+      if (current_covariate == "aspect") {
+        aspect_breaks <- seq(0, 2 * pi, by = pi / 2)
+        aspect_labels <- aspect_breaks * (180 / pi) - 180
+
+        plot[[n]] <- plot[[n]] +
+          scale_x_continuous(name = "aspect (°)", breaks = aspect_breaks, labels = aspect_labels)
+      }
+      if (current_covariate == "slope") {
+        slope_range <- range(model4plotting$model$slope, na.rm = TRUE)
+        slope_degree_breaks <- seq(0, 30, by = 5)
+        slope_breaks <- tan(slope_degree_breaks * (pi / 180))
+        slope_labels <- slope_degree_breaks
+
+        plot[[n]] <- plot[[n]] +
+          scale_x_continuous(name = "slope (°)", breaks = slope_breaks, labels = slope_labels)
+      }
+
+      plot[[n]] <- plot[[n]] +
+        ylab(sprintf("s(%s, %.2f)", current_covariate, edf_values[n+1])) +
+        # labs(caption = NULL) +
+        labs(title = NULL) +
+        geom_hline(yintercept = 0, linetype = "dashed", color = "black") +
+        theme_minimal() +
+        theme(panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank(),
+              panel.border = element_rect(color = "black", fill = NA, linewidth = 1)
+        )
+
+      # Log-scale for log-transformed covariates
+      if(current_covariate %in% log1p_trans){
+        plot[[n]] <- plot[[n]] +
+          scale_x_continuous(trans = "log1p")
+      }
+
+      # Add units for each covariate
+      if(current_covariate == "EKE"){
+        plot[[n]]$labels$x = paste0(plot[[n]]$labels$x, " (m/s)")
+      }
+      if(current_covariate == "NPPV"){
+        plot[[n]]$labels$x = bquote(.(plot[[n]]$labels$x) ~ "(mg/m"^3*"/day)")
+      }
+      # if(current_covariate %in% c("slope","aspect")){
+      #   plot[[n]]$labels$x = paste0(plot[[n]]$labels$x, " (°)")
+      # }
+      if(current_covariate %in% c("mSST","gradSST")){
+        plot[[n]]$labels$x = paste0(plot[[n]]$labels$x, " (°C)")
+      }
+      if(current_covariate %in% c("dist_coast","dist_50m","dist_200m","dist_2000m")){
+        plot[[n]]$labels$x = paste0(plot[[n]]$labels$x, " (m)")
+      }
+      if(current_covariate == "bathy"){
+        plot[[n]]$labels$x = "depth (m)"
+      }
+    }
+
+    out <- plot
+  }
+
   ##############
   chunk_modeli_p1 <- quote({
     for (i in 1:length(run_models$best_models)) {
@@ -268,10 +345,22 @@ model_comparison <- function(run_models, # output from run_all_DSM
         for (j in which_soap) {
           (plot(run_models$best_models[[i]], select = j))
         }
-        print(gratia::draw(run_models$best_models4plotting[[i]], rug = F, select = -which_soap))
+
+        spline_plot <- build_spline_plot(run_models$best_models[[i]], run_models$best_models4plotting[[i]], which_soap, log1p_trans)
+        print(spline_plot)
+
+        if (i == best_stacking & save_plots) {
+          if (!dir.exists(paste0(prediction_folder, "/Plots/", output_file))) {
+            dir.create(paste0(prediction_folder, "/Plots/", output_file))
+          }
+
+          ggsave(paste0(prediction_folder, "/Plots/", output_file, "/Spline Plot.png"), plot = spline_plot,
+                 width = 9, height = 5, dpi = 300)
+        }
+
       } else {
         if (grepl("t2\\(X,Y,", paste(stringr::str_remove_all(as.character(run_models[["best_models"]][[1]][["formula"]]),
-                                                               " "),
+                                                             " "),
                                      collapse = ""))) {
           print(gratia::draw(run_models$best_models4plotting[[i]], rug = F, select = 1))
           print(gratia::draw(run_models$best_models4plotting[[i]], rug = F, select = 2))
@@ -285,6 +374,7 @@ model_comparison <- function(run_models, # output from run_all_DSM
           print(gratia::draw(run_models$best_models4plotting[[i]], rug = F))
         }
       }
+
       mgcv::qq.gam(run_models$best_models[[i]], rep = 1000)
 
       cat("\n\n")
@@ -317,15 +407,30 @@ model_comparison <- function(run_models, # output from run_all_DSM
                        Value = "Predicted value")) %>%
         mutate(Value = as.factor(Value))
 
-      rootg <- ggplot2::ggplot() +
-        ggplot2::geom_histogram(data = checks %>%
-                                  dplyr::filter(as.character(Value) == "Observed value"), ggplot2::aes(x = new, fill = Value), alpha = 1, binwidth = 1) +
-        ggplot2::scale_fill_manual(values = viridis::viridis(256)[1]) +
-        ggnewscale::new_scale_fill() +
-        ggplot2::geom_histogram(data = checks %>%
-                                  dplyr::filter(as.character(Value) == "Predicted value"), ggplot2::aes(x = new, fill = Value), alpha = .5, binwidth = 1) +
-        ggplot2::scale_fill_manual(values = viridis::viridis(256)[256]) +
-        ggplot2::theme(legend.title = ggplot2::element_blank())
+      color1 <- viridis::viridis(256)[1]
+      color2 <- viridis::viridis(256)[256]
+
+      rgb1 <- col2rgb(color1) / 255
+      rgb2 <- col2rgb(color2) / 255
+      blended_rgb <- (rgb1 + rgb2) / 2
+      blended_color <- rgb(blended_rgb[1], blended_rgb[2], blended_rgb[3])
+
+      rootg <- ggplot() +
+        geom_histogram(data = checks %>%
+                         dplyr::filter(as.character(Value) == "Observed value"), aes(x = new, fill = Value), alpha = 1, binwidth = 1) +
+        scale_fill_manual(values = color1) +
+        new_scale_fill() +
+        geom_histogram(data = checks %>%
+                         dplyr::filter(as.character(Value) == "Predicted value"), aes(x = new, fill = Value), alpha = .5, binwidth = 1) +
+        scale_fill_manual(values = color2) +
+        new_scale_fill() +
+        geom_tile(aes(x = Inf, y = Inf, fill = "Observed + Predicted value")) +
+        scale_fill_manual(name = "",
+                          values = c("Observed + Predicted value" = blended_color)) +
+        scale_y_sqrt() +
+        ylab("Count") +
+        xlab("Number of individuals") +
+        theme(legend.title = element_blank())
 
       if (max(checks$new, na.rm = T) > 20) {
         rootg <- rootg +
@@ -333,8 +438,35 @@ model_comparison <- function(run_models, # output from run_all_DSM
       }
 
       print(rootg)
+
+      if (i == best_stacking & save_plots) {
+        ggsave(paste0(prediction_folder, "/Plots/", output_file, "/Rootogram Plot.png"), plot = rootg,
+               width = 8, height = 5, dpi = 300)
+      }
+
     }
   })
+
+
+  if (save_plots) {
+    i <- best_stacking
+    which_soap <- which(vapply(run_models$best_models[[i]]$smooth, inherits, logical(1), what = "soap.film"))
+
+    for (j in which_soap) {
+      if (!dir.exists(paste0(prediction_folder, "/Plots/", output_file))) {
+        dir.create(paste0(prediction_folder, "/Plots/", output_file))
+      }
+
+      png(paste0(prediction_folder, "/Plots/", output_file, "/Soap Plot.png"), width = 2000, height = 2000, res = 300)
+      plot(run_models$best_models[[i]], select = j)
+      dev.off()
+    }
+
+    png(filename = paste0(prediction_folder, "/Plots/", output_file, "/QQ Plot.png"),
+        width = 1500, height = 1500, res = 300)
+    mgcv::qq.gam(run_models$best_models[[i]], rep = 1000)
+    dev.off()
+  }
 
   rmd_text <- c(
     rmd_text,
@@ -792,8 +924,27 @@ model_comparison <- function(run_models, # output from run_all_DSM
                                                 ifelse(!is.na(corr_groupsize) & corr_groupsize != 1,
                                                        paste0("\nmultiplied by correction factor = ", corr_groupsize),
                                                        "")))))
-    # }
+
+    sightings <- calibdata %>%
+      filter(.data[[response]] > 0) %>%
+      st_as_sf(coords = c("X","Y"), crs = 3035)
+
+    sightings_plot <- ggplot2::ggplot() +
+      ggplot2::geom_sf(data = final %>%
+                         dplyr::filter(model == "model1") %>%
+                         dplyr::filter(areakm2 > 0) %>%
+                         dplyr::filter(!is.na(Avg_density)) %>%
+                         mutate(Avg_density = Avg_density * groupsizes_pred), ggplot2::aes(fill = cut(Avg_density,
+                                                                                                      breaks = breaks_plot,
+                                                                                                      labels = labels_plot
+                         )), color = NA) +
+      ggplot2::scale_fill_viridis_d(drop = F, name = "Average density\n(ind/km2)") +
+      ggplot2::labs(title = "Prediction with sightings") +
+      geom_sf(data = sightings, color = "red", size = 1, shape = 20, alpha = 1)
+
+    print(sightings_plot)
   })
+
 
   rmd_text <- c(
     rmd_text,
@@ -1598,6 +1749,7 @@ model_comparison <- function(run_models, # output from run_all_DSM
           ggplot2::labs(title = paste0("Model ", i, ": Bias"))
 
         print(bias_plot)
+
       }
     })
 
@@ -1622,17 +1774,17 @@ model_comparison <- function(run_models, # output from run_all_DSM
     quiet = F
   )
 
-  tryCatch(
-    rmarkdown::render(
-      input = tmp_md,
-      output_file = file.path(prediction_folder, paste0(output_file, ".html")),
-      quiet = FALSE
-    ),
-    error = function(e) {
-      print(e)
-      traceback()
-    }
-  )
+  # tryCatch(
+  #   rmarkdown::render(
+  #     input = tmp_md,
+  #     output_file = file.path(prediction_folder, paste0(output_file, ".html")),
+  #     quiet = FALSE
+  #   ),
+  #   error = function(e) {
+  #     print(e)
+  #     traceback()
+  #   }
+  # )
 
   message("HTML report generated: ", paste0(prediction_folder, "/", output_file, ".html"))
   browseURL(paste0(prediction_folder, "/", output_file, ".html"))
